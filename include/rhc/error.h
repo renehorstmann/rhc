2007f70795cffbc429ab8558758ed6433e7408f5
René Horstmann <r.horsty@gmail.com>
2021-04-16 23:00:49 +0200

    Update error.h

--- include/rhc/error.h
+++ include/rhc/error.h
@@ -12,11 +12,9 @@ extern _Thread_local const char *rhc_error;
 // functions
 //
 
-/**
- * assert like function, that also uses formatting print to stderr.
- * Calls raise(SIG_ABRT).
- * If NDEBUG is defined, only msg will get displayed (without expression, file and line infos)
- */
+// assert like function, that also uses formatting print to stderr.
+// Calls raise(SIG_ABRT).
+// If NDEBUG is defined, only msg will get displayed (without expression, file and line infos)
 #define assume(EX, ...) \
 (void)((EX) || (rhc_assume_impl_ (#EX, __FILE__, __LINE__, __VA_ARGS__),0))
 
