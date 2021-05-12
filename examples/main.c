6a2e11934ff3225982938344f19725f01b4e75de
renehorstmann <r.horsty@gmail.com>
2021-05-12 10:21:57 +0200

    log update

--- examples/main.c
+++ examples/main.c
@@ -1,8 +1,15 @@
+
+//#define RHC_LOG_DO_NOT_PRINT_TIME_FILE
 #include "rhc/rhc_impl.h"
 
 
 int main() {
+    log_trace("hey");
+    log_debug("hey");
     log_info("hey");
+    log_warn("hey");
+    log_error("hey");
+    log_wtf("hey");
 
 }
 
