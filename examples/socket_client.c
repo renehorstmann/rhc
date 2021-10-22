e05eb7d8d5134eaa31f8092e924056591b3baa27
René Horstmann <r.horsty@gmail.com>
2021-10-22 17:45:10 +0200

    Update socket_client.c

--- examples/socket_client.c
+++ examples/socket_client.c
@@ -5,13 +5,16 @@ int main() {
     puts("creating the connection");
     Socket *socket = socket_new("127.0.0.1", 12345);
 
+    // returns socket->stream or an invalid Stream_i, if socket is NULL
+    Stream_i ss = socket_get_stream(socket);
+
     char msg[32];
     sprintf(msg, "Hello Server");
     printf("write msg: <%32s>\n", msg);
-    stream_write_msg(socket->stream, msg, 32);
+    stream_write_msg(ss, msg, 32);
 
     puts("reading");
-    stream_read_msg(socket->stream, msg, 32);
+    stream_read_msg(ss, msg, 32);
     printf("read msg: <%32s>\n", msg);
 
     puts("killing socket");
