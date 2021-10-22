4f3dfc024878588c7a25e99507131a5cd84a0977
René Horstmann <r.horsty@gmail.com>
2021-10-22 17:42:27 +0200

    Update socket_server.c

--- examples/socket_server.c
+++ examples/socket_server.c
@@ -9,15 +9,18 @@ int main() {
     puts("accepting the client");
     Socket *client = socketserver_accept(&server);
 
+    // returns client->stream, or an invalid Stream_i, if client is NULL
+    Stream_i cs = socket_get_stream(client);
+
     puts("read a msg");
     char msg[32];
-    stream_read_msg(client->stream, msg, 32);
+    stream_read_msg(cs, msg, 32);
     printf("read msg: <%32s>\n", msg);
 
     str_toupper((Str_s) {msg, 32});
 
     printf("write msg: <%32s>\n", msg);
-    stream_write_msg(client->stream, msg, 32);
+    stream_write_msg(cs, msg, 32);
 
     puts("killing sockets");
     socket_kill(&client);
