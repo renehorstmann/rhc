e00321fc308047f7d09450e0b5107c97230d6f37
renehorstmann <r.horsty@gmail.com>
2022-08-02 11:58:01 +0200

    endian update (going back in time...)

--- template/in/rhc/endian.h
+++ template/in/rhc/endian.h
@@ -25,125 +25,150 @@ static void rhc_endian_swap(void *buf, int n) {
     memcpy(buf_data, tmp, n);
 }
 
-static void rhc_endian_host_to_le16(void *le16, const void *host16) {
-    rhcu16 h = *(const rhcu16*) host16;
-    rhcu8 *x = (rhcu8 *) le16;
-    x[1] = (h >> 8) & 0xFF;
-    x[0] = h & 0xFF;
+static rhcu16 rhc_endian_host_to_be16(rhcu16 x) {
+    rhcu16 result;
+    rhcu8 *c;
+    c = (rhcu8 *) &result;
+    c[0] = (x >> 8) & 0xFF;
+    c[1] = x & 0xFF;
+    return result;
 }
 
-static void rhc_endian_host_to_be16(void *be16, const void *host16) {
-    rhcu16 h = *(const rhcu16*) host16;
-    rhcu8 *x = (rhcu8 *) be16;
-    x[0] = (h >> 8) & 0xFF;
-    x[1] = h & 0xFF;
+static rhcu16 rhc_endian_host_to_le16(rhcu16 x) {
+    rhcu16 result;
+    rhcu8 *c;
+    c = (rhcu8 *) &result;
+    c[1] = (x >> 8) & 0xFF;
+    c[0] = x & 0xFF;
+    return result;
 }
 
-static void rhc_endian_le_to_host16(void *host16, const void *le16) {
-    rhcu16 h = *(rhcu16*) host16;
-    const rhcu8 *x = (const rhcu8 *) le16;
-    h = (rhcu16) x[0];
-    h |= (rhcu16) x[1] << 8;
+static rhcu16 rhc_endian_le_to_host16(rhcu16 x) {
+    rhcu16 result;
+    rhcu8 *c;
+
+    c = (rhcu8 *) &x;
+    result = (rhcu16) c[0];
+    result |= (rhcu16) c[1] << 8;
+    return result;
 }
 
-static void rhc_endian_be_to_host16(void *host16, const void *be16) {
-    rhcu16 h = *(rhcu16*) host16;
-    const rhcu8 *x = (const rhcu8 *) be16;
-    h = (rhcu16) x[1];
-    h |= (rhcu16) x[0] << 8;
+static rhcu16 rhc_endian_be_to_host16(rhcu16 x) {
+    rhcu16 result;
+    rhcu8 *c;
+
+    c = (rhcu8 *) &x;
+    result = (rhcu16) c[1];
+    result |= (rhcu16) c[0] << 8;
+    return result;
 }
 
-
-
-static void rhc_endian_host_to_le32(void *le32, const void *host32) {
-    rhcu32 h = *(const rhcu32*) host32;
-    rhcu8 *x = (rhcu8 *) le32;
-    x[3] = (h >> 24) & 0xFF;
-    x[2] = (h >> 16) & 0xFF;
-    x[1] = (h >> 8) & 0xFF;
-    x[0] = h & 0xFF;
+static rhcu32 rhc_endian_host_to_be32(rhcu32 x) {
+    rhcu32 result;
+    rhcu8 *c;
+    c = (rhcu8 *) &result;
+    c[0] = (x >> 24) & 0xFF;
+    c[1] = (x >> 16) & 0xFF;
+    c[2] = (x >> 8) & 0xFF;
+    c[3] = x & 0xFF;
+    return result;
 }
 
-static void rhc_endian_host_to_be32(void *be32, const void *host32) {
-    rhcu32 h = *(const rhcu32*) host32;
-    rhcu8 *x = (rhcu8 *) be32;
-    x[0] = (h >> 24) & 0xFF;
-    x[1] = (h >> 16) & 0xFF;
-    x[2] = (h >> 8) & 0xFF;
-    x[3] = h & 0xFF;
+static rhcu32 rhc_endian_host_to_le32(rhcu32 x) {
+    rhcu32 result;
+    rhcu8 *c;
+    c = (rhcu8 *) &result;
+    c[3] = (x >> 24) & 0xFF;
+    c[2] = (x >> 16) & 0xFF;
+    c[1] = (x >> 8) & 0xFF;
+    c[0] = x & 0xFF;
+    return result;
 }
 
-static void rhc_endian_le_to_host32(void *host32, const void *le32) {
-    rhcu32 h = *(rhcu32*) host32;
-    const rhcu8 *x = (const rhcu8 *) le32;
-    h = (rhcu32) x[0];
-    h |= (rhcu32) x[1] << 8;
-    h |= (rhcu32) x[2] << 16;
-    h |= (rhcu32) x[3] << 24;
+static rhcu32 rhc_endian_le_to_host32(rhcu32 x) {
+    rhcu32 result;
+    rhcu8 *c;
+
+    c = (rhcu8 *) &x;
+    result = (rhcu32) c[0];
+    result |= (rhcu32) c[1] << 8;
+    result |= (rhcu32) c[2] << 16;
+    result |= (rhcu32) c[3] << 24;
+    return result;
 }
 
-static void rhc_endian_be_to_host32(void *host32, const void *be32) {
-    rhcu32 h = *(rhcu32*) host32;
-    const rhcu8 *x = (const rhcu8 *) be32;
-    h = (rhcu32) x[3];
-    h |= (rhcu32) x[2] << 8;
-    h |= (rhcu32) x[1] << 16;
-    h |= (rhcu32) x[0] << 24;
+static rhcu32 rhc_endian_be_to_host32(rhcu32 x) {
+    rhcu32 result;
+    rhcu8 *c;
+
+    c = (rhcu8 *) &x;
+    result = (rhcu32) c[3];
+    result |= (rhcu32) c[2] << 8;
+    result |= (rhcu32) c[1] << 16;
+    result |= (rhcu32) c[0] << 24;
+    return result;
 }
 
-
-
-static void rhc_endian_host_to_le64(void *le64, const void *host64) {
-    rhcu64 h = *(const rhcu64*) host64;
-    rhcu8 *x = (rhcu8 *) le64;
-    x[7] = (h >> 56) & 0xFF;
-    x[6] = (h >> 48) & 0xFF;
-    x[5] = (h >> 40) & 0xFF;
-    x[4] = (h >> 32) & 0xFF;
-    x[3] = (h >> 24) & 0xFF;
-    x[2] = (h >> 16) & 0xFF;
-    x[1] = (h >> 8) & 0xFF;
-    x[0] = h & 0xFF;
+static rhcu64 rhc_endian_host_to_be64(rhcu64 x) {
+    rhcu64 result;
+    rhcu8 *c;
+    c = (rhcu8 *) &result;
+    c[0] = x >> 56;
+    c[1] = (x >> 48) & 0xFF;
+    c[2] = (x >> 40) & 0xFF;
+    c[3] = (x >> 32) & 0xFF;
+    c[4] = (x >> 24) & 0xFF;
+    c[5] = (x >> 16) & 0xFF;
+    c[6] = (x >> 8) & 0xFF;
+    c[7] = x & 0xFF;
+    return result;
 }
 
-static void rhc_endian_host_to_be64(void *be64, const void *host64) {
-    rhcu64 h = *(const rhcu64*) host64;
-    rhcu8 *x = (rhcu8 *) be64;
-    x[0] = (h >> 56) & 0xFF;
-    x[1] = (h >> 48) & 0xFF;
-    x[2] = (h >> 40) & 0xFF;
-    x[3] = (h >> 32) & 0xFF;
-    x[4] = (h >> 24) & 0xFF;
-    x[5] = (h >> 16) & 0xFF;
-    x[6] = (h >> 8) & 0xFF;
-    x[7] = h & 0xFF;
+static rhcu64 rhc_endian_host_to_le64(rhcu64 x) {
+    rhcu64 result;
+    rhcu8 *c;
+    c = (rhcu8 *) &result;
+    c[7] = x >> 56;
+    c[6] = (x >> 48) & 0xFF;
+    c[5] = (x >> 40) & 0xFF;
+    c[4] = (x >> 32) & 0xFF;
+    c[3] = (x >> 24) & 0xFF;
+    c[2] = (x >> 16) & 0xFF;
+    c[1] = (x >> 8) & 0xFF;
+    c[0] = x & 0xFF;
+    return result;
 }
 
-static void rhc_endian_le_to_host64(void *host64, const void *le64) {
-    rhcu64 h = *(rhcu64*) host64;
-    const rhcu8 *x = (const rhcu8 *) le64;
-    h = (rhcu64) x[0];
-    h |= (rhcu64) x[1] << 8;
-    h |= (rhcu64) x[2] << 16;
-    h |= (rhcu64) x[3] << 24;
-    h |= (rhcu64) x[4] << 32;
-    h |= (rhcu64) x[5] << 40;
-    h |= (rhcu64) x[6] << 48;
-    h |= (rhcu64) x[7] << 56;
+static rhcu64 rhc_endian_le_to_host64(rhcu64 x) {
+    rhcu64 result;
+    rhcu8 *c;
+
+    c = (rhcu8 *) &x;
+    result = (rhcu64) c[0];
+    result |= (rhcu64) c[1] << 8;
+    result |= (rhcu64) c[2] << 16;
+    result |= (rhcu64) c[3] << 24;
+    result |= (rhcu64) c[4] << 32;
+    result |= (rhcu64) c[5] << 40;
+    result |= (rhcu64) c[6] << 48;
+    result |= (rhcu64) c[7] << 56;
+    return result;
 }
 
-static void rhc_endian_be_to_host64(void *host64, const void *be64) {
-    rhcu64 h = *(rhcu64*) host64;
-    const rhcu8 *x = (const rhcu8 *) be64;
-    h = (rhcu64) x[7];
-    h |= (rhcu64) x[6] << 8;
-    h |= (rhcu64) x[5] << 16;
-    h |= (rhcu64) x[4] << 24;
-    h |= (rhcu64) x[3] << 32;
-    h |= (rhcu64) x[2] << 40;
-    h |= (rhcu64) x[1] << 48;
-    h |= (rhcu64) x[0] << 56;
-}
+static rhcu64 rhc_endian_be_to_host64(rhcu64 x) {
+    rhcu64 result;
+    rhcu8 *c;
 
+    c = (rhcu8 *) &x;
+    result = (rhcu64) c[7];
+    result |= (rhcu64) c[6] << 8;
+    result |= (rhcu64) c[5] << 16;
+    result |= (rhcu64) c[4] << 24;
+    result |= (rhcu64) c[3] << 32;
+    result |= (rhcu64) c[2] << 40;
+    result |= (rhcu64) c[1] << 48;
+    result |= (rhcu64) c[0] << 56;
+    return result;
+}
 
 #endif //RHC_ENDIAN_H
