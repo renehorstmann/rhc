72994cb1145e6abe6ab90c583285dcdf13672a21
renehorstmann <r.horsty@gmail.com>
2022-08-04 16:34:26 +0200

    export update

--- template/in/rhc/export.h
+++ template/in/rhc/export.h
@@ -5,10 +5,16 @@
 // to mark functions as library export function
 //
 
+#ifdef RHC_EXPORT_AS_LIBRARY
+
 #if (defined _WIN32 || defined WINCE || defined __CYGWIN__)
 #define RHC_EXPORT __declspec(dllexport)
 #elif defined __GNUC__ && __GNUC__ >= 4
 #define RHC_EXPORT __attribute__ ((visibility ("default")))
+#else
+#define RHC_EXPORT
+#endif
+
 #else
 #define RHC_EXPORT
 #endif
